8084a2874bb1df26b610594c4dea85d1ae9dc73d
Amaro Adonis Moraes Amorim <amaro.adonis@gmail.com>
2023-02-20 00:21:49 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,9 +9,12 @@ import model.entities.Seller;
 import java.sql.SQLOutput;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -40,6 +43,15 @@ public class Program {
         seller.setName("Martha Wayne");
         sellerDao.update(seller);
         System.out.println("Update completed!");
+
+        System.out.println("\n==== TEST 6: seller delete ====");
+        System.out.println("Enter id for  delete test: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed!");
+
+        sc.close();
 
     }
 }
