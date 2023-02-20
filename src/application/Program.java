cb2f3e4984b254be36298eef47558c778b17d63f
Amaro Adonis Moraes Amorim <amaro.adonis@gmail.com>
2023-02-20 00:13:04 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,5 +34,12 @@ public class Program {
         Seller newSeller = new Seller(null,"Greg","greg@email.com",new Date(),4.000,department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n==== TEST 5: seller update ====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
+
     }
 }
