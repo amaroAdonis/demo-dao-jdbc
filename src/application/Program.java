2d8216bf96a2e61926db591c24cb247cbee40f85
Amaro Adonis Moraes Amorim <amaro.adonis@gmail.com>
2023-02-19 21:47:55 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import model.dao.SellerDao;
 
 import model.entities.Seller;
 
+import java.sql.SQLOutput;
 import java.util.Date;
 
 public class Program {
@@ -13,6 +14,7 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
+        System.out.println("==== TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
